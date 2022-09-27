53ce5d8a0f4ed1796c7bba0aa6cdfb7e1932e7a1
p14041999 <p14041999@gmail.com>
2022-09-27 13:29:32 +0530

    Update token.ts

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -44,7 +44,7 @@ export class Token extends Currency {
    * @throws if the tokens are on different chains
    */
   public sortsBefore(other: Token): boolean {
-    invariant(this.chainId === other.chainId, 'CHAIN_IDS')
+    // invariant(this.chainId === other.chainId, 'CHAIN_IDS')
     invariant(this.address !== other.address, 'ADDRESSES')
     return this.address.toLowerCase() < other.address.toLowerCase()
   }
